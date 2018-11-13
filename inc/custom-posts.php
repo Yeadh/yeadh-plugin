acb2158cb2f9ec7556e442fff4a2965288789aff
Farhan Khan <farhan01617@gmail.com>
2018-11-13 15:14:16 +0600

    update

--- inc/custom-posts.php
+++ inc/custom-posts.php
@@ -1,35 +1,35 @@
 <?php
 
-if ( ! function_exists('beloon_custom_post_type') ) {
+if ( ! function_exists('yeadh_custom_post_type') ) {
 	
     /**
      * Register a custom post type.
      *
      * @link http://codex.wordpress.org/Function_Reference/register_post_type
      */
-    function beloon_custom_post_type() {
+    function yeadh_custom_post_type() {
 
         //portfolio
         register_post_type(
             'portfolio', array(
             'labels'                 => array(
-                'name'               => _x( 'Portfolio', 'post type general name', 'beloon' ),
-                'singular_name'      => _x( 'Portfolio', 'post type singular name', 'beloon' ),
-                'menu_name'          => _x( 'Portfolio', 'admin menu', 'beloon' ),
-                'name_admin_bar'     => _x( 'Portfolio', 'add new on admin bar', 'beloon' ),
-                'add_new'            => _x( 'Add New', 'Portfolio', 'beloon' ),
-                'add_new_item'       => __( 'Add New Portfolio', 'beloon' ),
-                'new_item'           => __( 'New Portfolio', 'beloon' ),
-                'edit_item'          => __( 'Edit Portfolio', 'beloon' ),
-                'view_item'          => __( 'View Portfolio', 'beloon' ),
-                'all_items'          => __( 'All Portfolio', 'beloon' ),
-                'search_items'       => __( 'Search Portfolio', 'beloon' ),
-                'parent_item_colon'  => __( 'Parent Portfolio:', 'beloon' ),
-                'not_found'          => __( 'No Portfolio found.', 'beloon' ),
-                'not_found_in_trash' => __( 'No Portfolio found in Trash.', 'beloon' )
+                'name'               => _x( 'Portfolio', 'post type general name', 'yeadh' ),
+                'singular_name'      => _x( 'Portfolio', 'post type singular name', 'yeadh' ),
+                'menu_name'          => _x( 'Portfolio', 'admin menu', 'yeadh' ),
+                'name_admin_bar'     => _x( 'Portfolio', 'add new on admin bar', 'yeadh' ),
+                'add_new'            => _x( 'Add New', 'Portfolio', 'yeadh' ),
+                'add_new_item'       => __( 'Add New Portfolio', 'yeadh' ),
+                'new_item'           => __( 'New Portfolio', 'yeadh' ),
+                'edit_item'          => __( 'Edit Portfolio', 'yeadh' ),
+                'view_item'          => __( 'View Portfolio', 'yeadh' ),
+                'all_items'          => __( 'All Portfolio', 'yeadh' ),
+                'search_items'       => __( 'Search Portfolio', 'yeadh' ),
+                'parent_item_colon'  => __( 'Parent Portfolio:', 'yeadh' ),
+                'not_found'          => __( 'No Portfolio found.', 'yeadh' ),
+                'not_found_in_trash' => __( 'No Portfolio found in Trash.', 'yeadh' )
             ),
 
-            'description'        => __( 'Description.', 'beloon' ),
+            'description'        => __( 'Description.', 'yeadh' ),
             'menu_icon'          => 'dashicons-layout',
             'public'             => true,
             'show_in_menu'       => true,
@@ -45,14 +45,14 @@ if ( ! function_exists('beloon_custom_post_type') ) {
             'portfolio',
             array(
                 'labels' => array(
-                    'name' => __( 'Portfolio Category', 'beloon' ),
-                    'add_new_item'      => __( 'Add New Category', 'beloon' ),
+                    'name' => __( 'Portfolio Category', 'yeadh' ),
+                    'add_new_item'      => __( 'Add New Category', 'yeadh' ),
                 ),
                 'hierarchical' => true,
                 'show_admin_column'     => true
         ));
     }
 
-    add_action( 'init', 'beloon_custom_post_type' );
+    add_action( 'init', 'yeadh_custom_post_type' );
 
 }
