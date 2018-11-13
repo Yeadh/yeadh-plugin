4657794c2ce705702d5c25a047c7a31ece98b21a
Farhan Khan <farhan01617@gmail.com>
2018-11-13 12:54:04 +0600

    update

--- bappy-helper.php
+++ bappy-helper.php
@@ -1,21 +1,17 @@
 <?php
 /**
-* Plugin Name: bappy plugin
-* Plugin URI: https://github.com/Yeadh/bappy-plugin
-* Description: After install the bappy WordPress Theme, you must need to install this "bappy-plugin" first to get all functions of bappy WP Theme.
+* Plugin Name: Yeadh Plugin
+* Plugin URI: https://github.com/Yeadh/yeadh-plugin
+* Description: After install the Yeadh WordPress Theme, you must need to install this "yeadh-plugin" first to get all functions of yeadh WP Theme.
 * Version: 1.0.0
 * Author: BusinessTheme
 * Author URI: https://github.com/Yeadh
-* Text Domain: bappy
+* Text Domain: yeadh
 * License: GPL/GNU.
 */
 
 
-//define
-define( 'PLG_URL', plugins_url() );
-define( 'PLG_DIR', dirname( __FILE__ ) ); 
-
 /**----------------------------------------------------------------*/
 /* Include all file
 /*-----------------------------------------------------------------*/  
-include_once(PLG_DIR. '/inc/custom-posts.php');
+include_once(dirname( __FILE__ ). '/inc/custom-posts.php');
